b903612f19c8358cfd377777e6671e0fb7633d38
Anna Petrova <ann.undine@gmail.com>
2024-09-12 18:44:17 +0200

    fix: implement correct slider description  (Thu, Sep 12, 2024 06:44:17 PM)

--- cssMemeSlider/script.js
+++ cssMemeSlider/script.js
@@ -1,10 +1,3 @@
-const slideDescription = [
-    'My dog be like:',
-    'When you learn German',
-    'Junior every day',
-    'Team work'
-]
-
 const controls = document.querySelectorAll('.control');
 const circleControls = document.querySelectorAll('.circle-control');
 let activecircleControl = 0;
@@ -13,8 +6,6 @@ let activecircleControl = 0;
 controls.forEach((element, index)=> {
     element.addEventListener('click', () => {
     document.querySelector('.slider-img').style.transform=`translateX(-${100 * index}%)`;
-    let memeDescription = document.querySelector('.meme-description');
-    memeDescription.textContent = slideDescription[index];
     circleControls[activecircleControl].classList.remove('checked');
     activecircleControl = index;
     circleControls[activecircleControl].classList.add('checked');
